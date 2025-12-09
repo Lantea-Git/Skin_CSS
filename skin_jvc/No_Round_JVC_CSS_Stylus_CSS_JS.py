e2dfe6d6be958a57b7cf2634a8df747d33509318
Atlantis <184443362+Lantea-Git@users.noreply.github.com>
2025-12-09 21:45:54 +0100

    Update No_Round_JVC_CSS_Stylus_CSS_JS.py

--- skin_jvc/No_Round_JVC_CSS_Stylus_CSS_JS.py
+++ skin_jvc/No_Round_JVC_CSS_Stylus_CSS_JS.py
@@ -6,55 +6,24 @@ js_file = "ui_2023_jvc_js.js"
 
 # === Motif des fichiers CSS Stylus
 pattern = "*JVC*Stylus_*.css"
-matches = [f for f in os.listdir('.') if fnmatch.fnmatch(f, pattern)]
+version_file = []
+for file in os.listdir('.'):
+    if fnmatch.fnmatch(file, pattern):
+        version_file.append(file)
 
-if matches:
-    css_file = max(matches, key=os.path.getmtime)  # Le plus récent
+if version_file:
+    css_file = max(version_file, key=os.path.getmtime)  # Le plus récent
 else:
     css_file = "ui_2023_jvc.user.css"  # Autre logique
 
 # === Lecture du CSS
-with open(css_file, "r", encoding="utf-8") as f:
-    css_content = f.read()
+with open(css_file, "r", encoding="utf-8") as file:
+    css_content = file.read()
 
-# === Métadonnées
+# === Get Métadonnées
 version = re.search(r"@version\s+([0-9.]+)", css_content).group(1)
 description = re.search(r"@description\s+([^.]+)", css_content).group(1)
 auteur = re.search(r"@author\s+(.+)", css_content).group(1)
-
-# === Nettoyage : en-têtes UserStyle + @-moz-document
-css_cleaned = re.sub(r"/\*\s*==UserStyle==[\s\S]*?==/UserStyle==\s*\*/", "", css_content)
-css_cleaned = re.sub(r"@-moz-document\s+domain\([^)]+\)\s*{", "", css_cleaned).strip()
-if css_cleaned.endswith("}"):
-    css_cleaned = css_cleaned[:-1].strip()
-
-#virer VAR-*
-lines = css_cleaned.splitlines()
-result = []
-i = 0
-while i < len(lines):
-    # Supprimer VCSS-START + la ligne suivante
-    if lines[i].strip() == "/* VCSS-START */":
-        i += 2  # saute START + la ligne après
-        continue
-
-    # Supprimer la ligne juste avant VCSS-END + la ligne VCSS-END elle-même
-    if lines[i].strip() == "/* VCSS-END */":
-        if result:
-            result.pop()  # retire la ligne précédente
-        i += 1  # saute la ligne VCSS-END
-        continue
-
-    result.append(lines[i])
-    i += 1
-
-css_cleaned = "\n".join(result)
-
-
-css_lines = css_cleaned.splitlines()
-if css_lines:
-    css_lines[0] = '    ' + css_lines[0]
-css_cleaned = "\n".join(css_lines)
 
 # === Génération du UserScript JS
 js_header = f"""// ==UserScript==
@@ -73,9 +42,41 @@ js_header = f"""// ==UserScript==
 /* SKIN CSS : https://userstyles.world/style/17542/ */
 """
 
-js_content = js_header + '\nGM_addStyle(`\n\n' + css_cleaned + '\n`);\n'
+# === Nettoyage : en-têtes UserStyle + @-moz-document
+css_content = re.sub(r"/\*\s*==UserStyle==[\s\S]*?==/UserStyle==\s*\*/\n?", "", css_content) #Entete
+css_content = re.sub(r"@-moz-document\s+domain\([^)]+\)\s*{", "", css_content) #moz 
+css_content = re.sub(r"}\s*$", "", css_content) #fin accolade css
 
-with open(js_file, "w", encoding="utf-8") as f:
-    f.write(js_content)
+
+css_result_split = []
+in_remove_block = False
+
+for line in css_content.splitlines(True):
+    stripped = line.strip()
+
+    # JSCSS-DEL-START - JSCSS-DEL-END block supprime
+    if "JSCSS-DEL-START" in stripped:
+        in_remove_block = True
+        continue
+    if "JSCSS-DEL-END" in stripped:
+        in_remove_block = False
+        continue
+    if in_remove_block:
+        continue
+
+    # JSCSS-ADD-START - JSCSS-ADD-END dans JSCSS
+    if "JSCSS-ADD-START" in stripped:
+        continue
+    if "JSCSS-ADD-END" in stripped:
+        continue
+
+    # Ajout des lignes normales ou internes au bloc UNCOMMENT
+    css_result_split.append(line)
+css_content = "".join(css_result_split)
+
+js_content = js_header + '\nGM_addStyle(`' + css_content + '\n`);\n'
+
+with open(js_file, "w", encoding="utf-8") as file:
+    file.write(js_content)
 
 print(f"✅ JS généré : {js_file}")
