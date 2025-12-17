be001bae8d517b306e60f42cb5cfe38aa6c67a54
Atlantis <184443362+Lantea-Git@users.noreply.github.com>
2025-12-17 03:47:16 +0100

    adapte regex
    adapte regex

--- skin_jvc/No_Round_JVC_CSS_Stylus_CSS_JS.py
+++ skin_jvc/No_Round_JVC_CSS_Stylus_CSS_JS.py
@@ -4,6 +4,7 @@ import fnmatch
 
 js_file = "ui_2023_jvc_js.js"
 
+
 # === Motif des fichiers CSS Stylus
 pattern = "*JVC*Stylus_*.css"
 version_file = []
@@ -25,6 +26,39 @@ version = re.search(r"@version\s+([0-9.]+)", css_content).group(1)
 description = re.search(r"@description\s+([^.]+)", css_content).group(1)
 auteur = re.search(r"@author\s+(.+)", css_content).group(1)
 
+
+
+# === Nettoyage : en-têtes UserStyle + @-moz-document
+css_content = re.sub(r"/\*\s*==UserStyle==[\s\S]*?==/UserStyle==\s*\*/\n?", "", css_content) #Entete
+# css_content = re.sub(r"@-moz-document\s+domain\([^)]+\)\s*{", "", css_content) #moz 
+# css_content = re.sub(r"}\s*$", "", css_content) #fin accolade css
+
+
+css_result_split = []
+in_remove_block = False
+
+for line in css_content.splitlines(True):
+    stripped = line.strip()
+
+    # JSCSS-DEL-BLOCK-START - JSCSS-DEL-BLOCK-END block supprime
+    if "JSCSS-DEL-BLOCK-START" in stripped:
+        in_remove_block = True
+        continue
+    if "JSCSS-DEL-BLOCK-END" in stripped:
+        in_remove_block = False
+        continue
+    if in_remove_block:
+        continue
+
+    # JSCSS-DEL-LINE dans JSCSS
+    if "JSCSS-DEL-LINE" in stripped:
+        continue
+
+    # Ajout des lignes normales ou internes au bloc UNCOMMENT
+    css_result_split.append(line)
+css_content = "".join(css_result_split)
+
+
 # === Génération du UserScript JS
 js_header = f"""// ==UserScript==
 // @name         UI_2023_JVC_JS
@@ -42,39 +76,7 @@ js_header = f"""// ==UserScript==
 /* SKIN CSS : https://userstyles.world/style/17542/ */
 """
 
-# === Nettoyage : en-têtes UserStyle + @-moz-document
-css_content = re.sub(r"/\*\s*==UserStyle==[\s\S]*?==/UserStyle==\s*\*/\n?", "", css_content) #Entete
-css_content = re.sub(r"@-moz-document\s+domain\([^)]+\)\s*{", "", css_content) #moz 
-css_content = re.sub(r"}\s*$", "", css_content) #fin accolade css
 
-
-css_result_split = []
-in_remove_block = False
-
-for line in css_content.splitlines(True):
-    stripped = line.strip()
-
-    # JSCSS-DEL-START - JSCSS-DEL-END block supprime
-    if "JSCSS-DEL-START" in stripped:
-        in_remove_block = True
-        continue
-    if "JSCSS-DEL-END" in stripped:
-        in_remove_block = False
-        continue
-    if in_remove_block:
-        continue
-
-    # JSCSS-ADD-START - JSCSS-ADD-END dans JSCSS
-    if "JSCSS-ADD-START" in stripped:
-        continue
-    if "JSCSS-ADD-END" in stripped:
-        continue
-
-    # Ajout des lignes normales ou internes au bloc UNCOMMENT
-    css_result_split.append(line)
-css_content = "".join(css_result_split)
-
-# js_content = js_header + '\nGM_addStyle(`' + css_content + '\n`);\n'
 js_content = js_header + f"""
 const style = document.createElement("style");
 style.textContent = `
